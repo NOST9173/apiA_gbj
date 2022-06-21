b2f7472b66bb16a194157f1a2f88798206063653
gimbeomjun <gimbeomjun@218.38.137.28>
2022-06-21 17:47:10 +0900

    mySQL 스키마 날라가서 다시 새로 작성.
    스키마.테이블이름 <-- 잊지 말기

--- jspstudy/src/main/java/jspstudy/dbconn/Dbconn.java
+++ jspstudy/src/main/java/jspstudy/dbconn/Dbconn.java
@@ -5,20 +5,43 @@ import java.sql.DriverManager;
 
 public class Dbconn {
 	
-		String url="jdbc:mysql://127.0.0.1:3306/opentutorials?serverTimezone=UTC&characterEncoding=UTF-8";
-		String user="root";
+	
+	/*
+		String url="jdbc:mysql:/beomjun9173.cafe24.com:3306/beomjun9173?serverTimezone=UTC&characterEncoding=UTF-8";
+		String user="beomjun9173";
 		String password = "dlfjsdlfjs9";		
 		
 		public Connection getConnection() {
 			Connection conn = null;
-			try {
+		try {
 			//
-			Class.forName("com.mysql.cj.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 			//
 			conn = DriverManager.getConnection(url, user, password);
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
-			return conn;
 		}
+	}
+	*/
+//
+
+
+	String url="jdbc:mysql://127.0.0.1:3306/sys?serverTimezone=UTC&characterEncoding=UTF-8";
+	String user="root";
+	String password = "dlfjsdlfjs9";		
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+		//
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//
+		conn = DriverManager.getConnection(url, user, password);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 }
+
